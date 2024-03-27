0dd50f83ee133070bbacaddbc34d8d9ec700d52f
Harshit M <harshitmeh111@gmail.com>
2024-03-27 16:47:26 +0530

    9

--- index.py
+++ index.py
@@ -11,35 +11,35 @@ def generate_random_number():
 
 # Main function executed by Appwrite
 def main(context):
-    # You can log messages to the console
-    context.log("Hello, Logs!")
-    context.log(context.req.body_raw)  # Raw request body, contains request data
-    context.log(json.dumps(context.req.body))  # Object from parsed JSON request body, otherwise string
-    context.log(json.dumps(context.req.headers))  # String key-value pairs of all request headers, keys are lowercase
-    context.log(context.req.scheme)  # Value of the x-forwarded-proto header, usually http or https
-    context.log(context.req.method)  # Request method, such as GET, POST, PUT, DELETE, PATCH, etc.
-    context.log(context.req.url)  # Full URL, for example: http://awesome.appwrite.io:8000/v1/hooks?limit=12&offset=50
-    context.log(context.req.host)  # Hostname from the host header, such as awesome.appwrite.io
-    context.log(context.req.port)  # Port from the host header, for example 8000
-    context.log(context.req.path)  # Path part of URL, for example /v1/hooks
-    context.log(context.req.query_string)  # Raw query params string. For example "limit=12&offset=50"
-    context.log(json.dumps(context.req.query))  # Parsed query params. For example, req.query.limit
+    try:
+        # You can log messages to the console
+        context.log("Hello, Logs!")
+        context.log(context.req.body_raw)  # Raw request body, contains request data
+        context.log(json.dumps(context.req.body))  # Object from parsed JSON request body, otherwise string
+        context.log(json.dumps(context.req.headers))  # String key-value pairs of all request headers, keys are lowercase
+        context.log(context.req.scheme)  # Value of the x-forwarded-proto header, usually http or https
+        context.log(context.req.method)  # Request method, such as GET, POST, PUT, DELETE, PATCH, etc.
+        context.log(context.req.url)  # Full URL, for example: http://awesome.appwrite.io:8000/v1/hooks?limit=12&offset=50
+        context.log(context.req.host)  # Hostname from the host header, such as awesome.appwrite.io
+        context.log(context.req.port)  # Port from the host header, for example 8000
+        context.log(context.req.path)  # Path part of URL, for example /v1/hooks
+        context.log(context.req.query_string)  # Raw query params string. For example "limit=12&offset=50"
+        context.log(json.dumps(context.req.query))  # Parsed query params. For example, req.query.limit
 
-    # If something goes wrong, log an error
-    context.error("Hello, Errors!")
+        print(environ)
 
-    # Parse the event data
-    print(environ)
+        r = redis.Redis(
+            host=environ.get("REDIS_HOST"), port=int(environ.get("REDIS_PORT")), password=environ.get("REDIS_PASSWORD"), decode_responses=True
+        )
 
-    r = redis.Redis(
-        host=environ.get("REDIS_HOST"), port=int(environ.get("REDIS_PORT")), password=environ.get("REDIS_PASSWORD"), decode_responses=True
-    )
+        file_id = json.loads(context.req.body_raw)["$id"]
 
-    # Get the file id
-    file_id = json.loads(context.req.body_raw)["$id"]
+        random_number = generate_random_number()
 
-    # Generate a random number
-    random_number = generate_random_number()
+        r.set(file_id, random_number)
 
-    # Set the file ID and random number in Redis
-    r.set(file_id, random_number)
+        return context.res.json({"status": "OK"})
+
+    except Exception as e:
+        conole.error(e)
+        return context.res.json({"status": "ERROR"})
